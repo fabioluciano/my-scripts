6855c2539016248ff8bb3c67e5c234b9f9044f14
Fábio Luciano <fabio@naoimporta.com>
2020-04-09 21:36:30 -0300

    fix: sort by name and print unique packages

--- util/dpkg.sh
+++ util/dpkg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 last_pkgs() {
-  grep $(date +'%Y-%m-%d') /var/log/dpkg.log | awk '$3 == "upgrade"'  | awk '{ gsub(/:[a-z0-9]+/, "", $4); printf "\033[37m%s \033[31m%s\033[32m %s\033[0m\n", $4, $5, $6 }' | column -t
+  grep $(date +'%Y-%m-%d') /var/log/dpkg.log | awk '$3 == "upgrade"'  | awk '{ gsub(/:[a-z0-9]+/, "", $4); printf "\033[37m%s \033[31m%s\033[32m %s\033[0m\n", $4, $5, $6 }' | sort -k1,1 --unique --stable | column -t 
 }
